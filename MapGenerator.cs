0efb80c913314a9e3ad4e54b86843bfeb3f6f4b0
Rest <Rest2015@users.noreply.github.com>
2016-07-07 16:27:11 +0800

    Add files via upload

--- MapGenerator.cs
+++ MapGenerator.cs
@@ -12,26 +12,64 @@ public class MapGenerator : MonoBehaviour {
     private int[,] _floorInfo;
     private int[,] _mapInfo;//0:none -1:wall N:room N
 
+	private class cCorridorExit {
+		public int X;
+		public int Z;
+		public int deltaX;
+		public int deltaZ;
+	}
+
     private class cRoom {
+		public cRoom (
+			int idValue,
+			int xValue,
+			int zValue,
+			int widthValue,
+			int lengthValue
+		) {
+			id = idValue;
+			x = xValue;
+			z = zValue;
+			xWidth = widthValue;
+			zLength = lengthValue;
+		}
         public int id;
         public int x;
         public int z;
         public int xWidth;
-        public int zLength;
-        public int exit;   
+        public int zLength; 
     }
 
     private class cCorridor {
+		public cCorridor (
+			int idValue,
+			int xValue,
+			int zValue,
+			int delX,
+			int delZ,
+			int corridorLength
+		) {
+			id = idValue;
+			x = xValue;
+			z = zValue;
+			deltaX = delX;
+			deltaZ = delZ;
+			exitList = new List<cCorridorExit>();
+		}
+		public int id;
         public int x;
         public int z;
-        public int endX;
-        public int endZ;
-        public int exit;
+        public int deltaX;
+        public int deltaZ;
+		public List<cCorridorExit> exitList;
     }
 
-    int currentRoomId = 0;
+	int currentRoomId;
+	int currentCorridorId;
     cRoom currentRoom;
+	cCorridor currentCorridor;
     List<cRoom> roomList;
+	List<cCorridor> corridorList;
 
     void Awake() {
         Create();
@@ -64,18 +102,19 @@ public class MapGenerator : MonoBehaviour {
 
     void GenerateMap() {
         currentRoomId = 1;
+		currentCorridorId = 1;
         roomList = new List<cRoom>();
 
-        currentRoom = new cRoom();
-        currentRoom.x = Random.Range(_mapX / 3, _mapX / 3 * 2);
-        currentRoom.z = Random.Range(_mapZ / 3, _mapZ / 3 * 2);
-        currentRoom.xWidth = Random.Range(_roomMinRange, _roomMaxRange);
-        currentRoom.zLength = Random.Range(_roomMinRange, _roomMaxRange);
-        currentRoom.id = currentRoomId;
+		currentRoom = new cRoom(
+			currentRoomId,
+			Random.Range(_mapX / 3, _mapX / 3 * 2),
+			Random.Range(_mapZ / 3, _mapZ / 3 * 2),
+			Random.Range(_roomMinRange, _roomMaxRange),
+			Random.Range(_roomMinRange, _roomMaxRange)
+		);
+
         UpdateMapInfo(currentRoom);
         roomList.Add(currentRoom);
-
-        
 
         for (int n=_expectRoomNum; n>0; n--) {
 
@@ -117,6 +156,57 @@ public class MapGenerator : MonoBehaviour {
     bool CreateCorridorRecursivly () {
         //randomly choose a room
         cRoom room = roomList[Random.Range(0, roomList.Count - 1)];
+		int deltaX, deltaZ;
+		int wallX, wallZ;//target wall pos
+		switch (Random.Range(1,4)) {
+		//randomly choose a direction (for corridor)
+		case 1:
+			//up
+			deltaX = 1;
+			deltaZ = 0;
+			wallX = Random.Range (room.x + 1, room.x + room.xWidth - 2);
+			wallZ = room.z + room.zLength - 1;
+			break;
+		case 2:
+			//down
+			deltaX = -1;
+			deltaZ = 0;
+			wallX = Random.Range (room.x + 1, room.x + room.xWidth - 2);
+			wallZ = room.z;
+			break;
+		case 3:
+			//left
+			deltaX = 0;
+			deltaZ = -1;
+			wallX = room.x;
+			wallZ = Random.Range (room.z + 1, room.z + room.zLength - 2);
+			break;
+		case 4:
+			//right
+			deltaX = 0;
+			deltaZ = 1;
+			wallX = room.x+room.xWidth-1;
+			wallZ = Random.Range (room.z + 1, room.z + room.zLength - 2);
+			break;
+		default:
+			deltaX = 0;
+			deltaZ = 0;
+			Debug.LogError ("Invalid Direction!");
+			break;
+		}
+		currentCorridor = new cCorridor (
+			currentRoomId,
+			wallX + deltaX,
+			wallZ + deltaZ,
+			deltaX,
+			deltaZ
+		);
+
+
         return true;
     }
+
+	bool CheckCorridorSpace (int startX, int startZ, int delX, int delZ, int corridorLength) {
+		
+	}
 }
